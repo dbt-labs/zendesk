5721dd207447e19b21560a8fcddd25046058f6db
Tristan Handy <jthandy@gmail.com>
2017-01-04 13:39:49 -0500

    updated tickets model

--- models/stitch/base/zendesk_tickets.sql
+++ models/stitch/base/zendesk_tickets.sql
@@ -1,2 +1,17 @@
-select *
-from zendesk.tickets
+select
+  id,
+  organization_id,
+  assignee_id,
+  brand_id,
+  group_id,
+  priority,
+  subject,
+  recipient,
+  requester_id,
+  status,
+  submitter_id,
+  "type",
+  via__channel as channel,
+  created_at,
+  updated_at
+from {{var('tickets_table')}}
