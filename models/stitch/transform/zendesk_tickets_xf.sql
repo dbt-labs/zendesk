32571e3276101d149031a35940230abbde1d8155
Erin Ogilvy <erin.ogilvy@gmail.com>
2019-01-02 17:01:07 -0500

    add organization to tickets

--- models/stitch/transform/zendesk_tickets_xf.sql
+++ models/stitch/transform/zendesk_tickets_xf.sql
@@ -16,6 +16,12 @@ events as (
 
 ),
 
+organizations as (
+
+  select * from {{ref('zendesk_organizations')}}
+
+),
+
 final as (
 
     select
@@ -25,11 +31,14 @@ final as (
         events.ticket_first_solved,
         events.ticket_last_solved,
         events.ticket_closed,
+        organizations.name as organization_name,
         'https://' || url.org_url_identifier || '.zendesk.com/agent/tickets/' 
             || tickets.ticket_id::varchar as web_url
     from tickets
     left join events 
         on events.ticket_id = tickets.ticket_id
+    left join organizations 
+        on organizations.organization_id = tickets.organization_id
     inner join url 
         on 1 = 1
         
