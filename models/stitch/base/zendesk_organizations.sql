d053637875b4516255779696932a52a8aa719c16
Erin Ogilvy <erin.ogilvy@gmail.com>
2019-01-08 15:42:54 -0500

    remove merge issue and add emails to tickets

--- models/stitch/base/zendesk_organizations.sql
+++ models/stitch/base/zendesk_organizations.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 with source as (
     
     select * from {{var('organizations_table')}}
@@ -24,16 +23,3 @@ renamed as (
 )
 
 select * from renamed
-=======
-select
-
-    id,
-    external_id,
-    name,
-    case when deleted_at is not null then true else false end as deleted,
-    created_at,
-    updated_at,
-    deleted_at
-    
-from {{var('organizations_table')}}
->>>>>>> f7c871205ec534bd751636052e91fd0f472bb03b
--- models/stitch/transform/zendesk_tickets_xf.sql
+++ models/stitch/transform/zendesk_tickets_xf.sql
@@ -1,24 +1,30 @@
 with tickets as (
 
-  select * from {{ref('zendesk_tickets')}}
+    select * from {{ref('zendesk_tickets')}}
 
 ), 
 
 url as (
 
-  select * from {{ref('zendesk_org_url')}}
+    select * from {{ref('zendesk_org_url')}}
 
 ),
 
 events as (
 
-  select * from {{ref('zendesk_ticket_events_flattened')}}
+    select * from {{ref('zendesk_ticket_events_flattened')}}
 
 ),
 
 organizations as (
 
-  select * from {{ref('zendesk_organizations')}}
+    select * from {{ref('zendesk_organizations')}}
+
+),
+
+users as (
+    
+    select * from {{ ref('zendesk_users') }}
 
 ),
 
@@ -32,6 +38,8 @@ final as (
         events.ticket_last_solved,
         events.ticket_closed,
         organizations.name as organization_name,
+        requesters.email as requester_email,
+        assignees.email as assignee_email,
         'https://' || url.org_url_identifier || '.zendesk.com/agent/tickets/' 
             || tickets.ticket_id::varchar as web_url
     from tickets
@@ -39,6 +47,10 @@ final as (
         on events.ticket_id = tickets.ticket_id
     left join organizations 
         on organizations.organization_id = tickets.organization_id
+    left join users as requesters
+        on requesters.user_id = tickets.requester_id
+    left join users as assignees
+        on assignees.user_id = tickets.assignee_id
     inner join url 
         on 1 = 1
         
